dcba2515801a4841f2d4fd349bc7901cc2ecc249
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-03-29 20:38:01 +0300

    change font to source code pro

--- themes/first/theme.lua
+++ themes/first/theme.lua
@@ -7,7 +7,7 @@ local themes_path = gfs.get_themes_dir()
 
 local theme = {}
 
-theme.font          = "inconsolata 12"
+theme.font          = "Source Code Pro 13"
 
 theme.bg_normal     = "#222222"
 theme.bg_focus      = "#535d6c"
