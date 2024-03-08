0fa84c471f1b8b9d659eaf06eec6779b77c2b182
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-03-08 21:02:16 +0200

    disable click for menu on desktop

--- rc.lua
+++ rc.lua
@@ -545,8 +545,8 @@ end)
 
 -- mouse bindings
 root.buttons(gears.table.join(
-    awful.button({ }, 1, function () mymainmenu:toggle() end),
-    awful.button({ }, 3, function () mymainmenu:hide() end)
+    -- awful.button({ }, 1, function () mymainmenu:toggle() end),
+    -- awful.button({ }, 3, function () mymainmenu:hide() end)
     --awful.button({ }, 4, awful.tag.viewnext),
     --awful.button({ }, 5, awful.tag.viewprev)
 ))
