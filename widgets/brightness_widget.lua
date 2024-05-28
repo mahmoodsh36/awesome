36b165977829a1c7ab6786d13660ec83e354ae0d
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2024-05-28 15:09:50 +0300

    use light for brightness

--- widgets/brightness_widget.lua
+++ widgets/brightness_widget.lua
@@ -1,12 +1,13 @@
 local slider_controlled_widget = require("widgets/slider_controlled_widget")
 local awful = require("awful")
 
-local MAX_BRIGHTNESS = 400
+-- local MAX_BRIGHTNESS = 400
 
 brightness_widget = slider_controlled_widget:new(' ')
 
 brightness_widget.on_slider_input = function(value)
-    awful.spawn('sudo sh -c \'echo "' .. tostring(math.floor(value / 100 * MAX_BRIGHTNESS)) .. '" > /sys/class/backlight/intel_backlight/brightness\'')
+    -- awful.spawn('sudo sh -c \'echo "' .. tostring(math.floor(value / 100 * MAX_BRIGHTNESS)) .. '" > /sys/class/backlight/intel_backlight/brightness\'')
+    awful.spawn('sudo sh -c \'light -S "' .. value .. '"\'')
 end
 
 return brightness_widget
